905edbab1afe001b90953b41e524ec40d925e181
alexkruk <alexkruk93@yandex.ru>
2021-02-21 06:25:01 +0300

    data load only for dev profile

--- src/main/java/org/akriuchk/minishop/MiniShopApplication.java
+++ src/main/java/org/akriuchk/minishop/MiniShopApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -29,6 +30,7 @@ public class MiniShopApplication {
     }
 
     @Bean
+    @Profile("dev")
     public CommandLineRunner demoData(ProductRepository repo,
                                       CategoriesRepository catRepo,
                                       ImageRepository imageRepo) {
